e03af3e45386c26e4ef834d20f4a3b19fac9b71e
Alex Safonov <me@asafonov.org>
2013-08-21 11:08:31 +0400

    Adding accessors to component. User model

--- classes/core/component.class.php
+++ classes/core/component.class.php
@@ -13,6 +13,26 @@ class component {
             }
         }
     }
+
+    public function __get($name) {
+        $method = 'get'.$name;
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        } elseif(property_exists($this, $name)) {
+            return $this->$name;
+        } else {
+            throw new RuntimeException("Error while getting property ".$name);
+        }
+    }
+
+    public function __set($name, $value) {
+        $method = 'set'.$name;
+        if (method_exists($this, $method)) {
+            $this->$method($value);
+        } else {
+            throw new RuntimeException("Error while setting property ".$name);
+        }
+    }
 }
 
 ?>
---
+++ classes/core/user.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class user extends activeRecord {
+
+    private $login;
+    private $password;
+    private $email;
+    private $name;
+    private $sex;
+    private $avatar;
+    private $photo;
+    private $country;
+    private $city;
+
+}
+
+?>
