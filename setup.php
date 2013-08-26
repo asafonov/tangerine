4ddca2e6e7d169dd64215e48033a66d5a7f91f93
Alex Safonov <me@asafonov.org>
2013-08-26 12:58:26 +0400

    Setting page layout

--- setup.php
+++ setup.php
@@ -7,6 +7,7 @@ $user->create(array('login'=>'demo', 'password'=>'demo', 'email'=>'demo@mailfors
 
 $page = new page();
 $page->url = '/';
+$page->layout = 'index';
 $page->blocks = array();
 $page->blocks['user_login'] = array('type'=>'user->login', 'data'=>'');
 $page->blocks['content'] = array('type'=>'text', 'data'=>'<h1>Main page</h1><p>This is the main page');
