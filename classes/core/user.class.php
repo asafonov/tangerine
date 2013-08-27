81cccf85595770399f269b26700337a2825c780e
Alex Safonov <me@asafonov.org>
2013-08-27 10:43:04 +0400

    Functions for checking login and email availability before registering the user

--- classes/core/user.class.php
+++ classes/core/user.class.php
@@ -23,8 +23,28 @@ class user extends activeRecord {
             $data = registry::getInstance()->getService('request')->query;
         }
         $data['password'] = isset($data['password'])?$this->_crypt->hash($data['password']):'';
+        if (!$this->id) {
+            $this->_checkEmail($data['email']);
+            $this->_checkLogin($data['login']);
+        }
         $this->init($data);
         $this->save();
+    }
+
+    private function _checkLogin($login) {
+        $user = new user();
+        $user->load(array('login'=>$login));
+        if ($user->id) {
+            throw new RuntimeException("Login already registered", 1);
+        }
+    }
+
+    private function _checkEmail($emailk) {
+        $user = new user();
+        $user->load(array('email'=>$email));
+        if ($user->id) {
+            throw new RuntimeException("Email already registered", 2);
+        }
     }
 
     public function login($login, $password) {
