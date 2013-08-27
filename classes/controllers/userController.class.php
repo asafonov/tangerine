f32ad56fc27175925d3cd516991e26daa5e27142
Alex Safonov <me@asafonov.org>
2013-08-27 11:37:52 +0400

    userController->regoster

--- classes/controllers/userController.class.php
+++ classes/controllers/userController.class.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 class userController extends baseController {
 
     public function isAuthorized() {
@@ -47,12 +49,62 @@ class userController extends baseController {
         return $this->_loginForm();
     }
 
-    public function registerForm() {
+    public function register() {
         if ($this->isAuthorized()) {
             $this->Location('/');
         }
-        $template = new template('userregisterForm');
-        return $template->fill();
+        if (count($this->query)>0) {
+            return $this->_processRegister();
+        } else {
+            return $this->_registerForm();
+        }
+    }
+
+    private function _registerForm($data = array()) {
+        $template = new template('user_registerForm');
+        $captcha_array = array('session_name'=>session_name(), 'session_id'=>session_id());
+        return $template->fill(array_merge($data, $captcha_array, $this->query));
+    }
+
+    private function _processRegister() {
+        try {
+            $this->_checkQuery();
+            registry::getInstance()->getService('user')->create();
+        } catch(Exception $e) {
+            return $this->_registerForm(array('error'.$e->getCode()=>true));
+        }
+    }
+
+    private function _checkQuery() {
+        $this->_checkEmpty();
+        $this->_checkPassword();
+        $this->_checkCaptcha();
+    }
+
+    private function _checkEmpty() {
+        if (!$this->query['login']==''&&
+            !$this->query['email']==''&&
+            !$this->query['password']=='') {
+            return true;
+        } else {
+            throw new Exception("Obligatory field is not filled", 4);
+        }
+    }
+
+    private function _checkPassword() {
+        if ($this->query['password']==$this->query['rpassword']) {
+            return true;
+        } else {
+            throw new Exception("Failed to repeat the password", 5);
+        }
+    }
+
+    private function _checkCaptcha() {
+        if (isset($_SESSION['captcha_keystring'])&&$_SESSION['captcha_keystring']==$this->query['keystring']) {
+            return true;
+        } else {
+            throw new Exception("Error in captcha keystring", 3);
+        }
     }
 }
 
